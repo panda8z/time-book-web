8814de62beab472587de5f2fdec39f287807de7f
panda8z <panda8@126.com>
2020-07-13 15:52:11 +0800

    Todoyinput类组件接受外部参数使用 propsTypes检查

--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+
 export default class TodoInput extends Component {
-  render() {
-    return (
-      <div>
-          <input type='text'/><button>{this.props.btnText}</button>
-      </div>
-    );
-  }
+
+    static defaultProps = {
+        btnText: '添加Todo'
+    }
+
+    static propsTypes = {
+        btnText: PropTypes.string
+    }
+    render() {
+        return (
+            <div>
+                <input type='text' /><button>{this.props.btnText}</button>
+            </div>
+        );
+    }
 }
