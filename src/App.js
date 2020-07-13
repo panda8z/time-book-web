279c814523cc2ed3b979705d5022a24042a676fd
panda8z <panda8@126.com>
2020-07-13 14:39:42 +0800

    使用framgment组件不会产生外部div包裹

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component , Fragment} from 'react';
 import {TodoHeader, TodoInput,TodoList }from './components';
 
 export default class App extends Component {
   render() {
     console.log(this.props)
-    return (<div className='body'>
+    return (<Fragment className='body'>
        <TodoHeader/>
        <TodoInput/>
        <TodoList/>
-    </div>);
+    </Fragment>);
   }
 }
 
