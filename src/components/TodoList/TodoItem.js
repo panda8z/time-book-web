c01c1674e23f84add21f0356861584eeec41426e
panda8z <panda8@126.com>
2020-07-13 17:05:39 +0800

    模板渲染的解释

--- src/components/TodoList/TodoItem.js
+++ src/components/TodoList/TodoItem.js
@@ -4,6 +4,7 @@ export default class TodoItem extends Component {
   render() {
     return (
       <li>
+          {/* 这部分语句就是模板，进来的数据props就是数据，不同的数据同样的样式模板。这就是模板渲染。 */}
           {
               this.props.title  + this.props.isCompeleted ? '已完成✅' : '未完成❎'
           }
