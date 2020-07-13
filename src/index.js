b128aafee2f44bd40ba0a54fb583a60a18eaf18b
panda8z <panda8@126.com>
2020-07-13 10:02:22 +0800

    类组件嵌套。

--- src/index.js
+++ src/index.js
@@ -7,10 +7,20 @@ class App extends React.Component {
 
     render() {
         console.log(this.props)
-        return (<div>
+        return (<div class='body'>
+            <Header headerTitle="Headddder"/>
             <h1>Welcom!</h1>
             {/* jsx里面的js代码用 {} 包裹 */}
             <p>{this.props.title}</p>
+        </div>)
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (<div class='header'>
+                <h2>I`m Header of this!</h2>
+    <p>{this.props.headerTitle}</p>
         </div>)
     }
 }
