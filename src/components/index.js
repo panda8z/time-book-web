433010a8a827063a134a32dabd61835e49547e63
panda8z <panda8@126.com>
2020-07-13 14:43:39 +0800

    另一种组件开发方式

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,17 @@
-import TodoHeader from "./TodoHeader";
-import TodoInput from "./TodoInput";
-import TodoList from "./TodoList";
+// 第一种到处方式
+// import TodoHeader from "./TodoHeader";
+// import TodoInput from "./TodoInput";
+// import TodoList from "./TodoList";
 
-export {
-    TodoHeader,
-    TodoInput,
-    TodoList,
-}
+// export {
+//     TodoHeader,
+//     TodoInput,
+//     TodoList,
+// }
+
+
+// 第二种导出方式
+
+export { default as TodoHeader } from './TodoHeader'
+export { default as TodoList } from './TodoList'
+export { default as TodoInput } from './TodoInput'
