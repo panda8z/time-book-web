0568bbe7579854708ceb364a3cbc894cb9c7d962
panda8z <panda8@126.com>
2020-07-13 09:41:09 +0800

    define jsx outside render func

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 import ReactDOM from 'react-dom'
 
+const app = <div>
+    <h1>Welcom!</h1>
+    <p>React</p>
+</div>
 ReactDOM.render(
-    <h1>Welcom!</h1>,
+    app,
     document.querySelector('#root')
 )
