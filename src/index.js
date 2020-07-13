b24bd1a0e39ce6866ef51187326f717e74d2f33b
panda8z <panda8@126.com>
2020-07-13 14:13:54 +0800

    定义样式的几种方式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import {render} from 'react-dom'
+import { render } from 'react-dom'
+
+import classNames from 'classnames'
 
 import './index.css'
 
@@ -8,11 +10,17 @@ class App extends React.Component {
 
     render() {
         console.log(this.props)
-        return (<div className='body' style={{backgroundColor: 'red',}}>
-            <Header headerTitle="Headddder"/>
+        return (<div className='body'>
+            <Header headerTitle="Headddder" />
             <h1>Welcom!</h1>
             {/* jsx里面的js代码用 {} 包裹 */}
             <p>{this.props.title}</p>
+
+            <ol>
+                <li style={{ color: '#f00', fontSize: '24px' }}>使用内联style属性定义样式</li>
+                <li className="has-text-red">使用class + css类选择器定义属性 class要写成className</li>
+                <li className={classNames('a',{'b':true}, {'c': false})}>使用classnames第三方库实现动态使用不同的class + css类选择器定义属性 class要写成className<br/>本例子中class应该只有a，b没有c</li>
+            </ol>
         </div>)
     }
 }
@@ -20,8 +28,8 @@ class App extends React.Component {
 class Header extends React.Component {
     render() {
         return (<div className='header'>
-                <h2>I`m Header of this!</h2>
-                <p>{this.props.headerTitle}</p>
+            <h2>I`m Header of this!</h2>
+            <p>{this.props.headerTitle}</p>
         </div>)
     }
 }
