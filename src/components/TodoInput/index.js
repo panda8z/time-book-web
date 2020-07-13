ba26d51400fdfa02d85aa1b3e55b8361bfcb829e
panda8z <panda8@126.com>
2020-07-13 14:46:30 +0800

    尝试进行组件 开发

--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class TodoInput extends Component {
   render() {
     return (
-      <div> TodoInput </div>
+      <div>
+          <input type='text'/><button>添加</button>
+      </div>
     );
   }
 }
