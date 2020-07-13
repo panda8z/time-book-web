0f56b385efb324efa3dfcb65552e7368d46d13f7
panda8z <panda8@126.com>
2020-07-13 09:47:26 +0800

    组件化的基本方法。

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // 这是一个箭头函数定义的方法。
-const createApp = (props) => { 
-    return <div>
+const App = (props) => { 
+    return (<div>
     <h1>Welcom!</h1>
     {/* jsx里面的js代码用 {} 包裹 */}
     <p>{props.title}</p>
-</div>
+</div>)
 }
 
-const app = createApp({ 
-    title: "React 16.8"
-})
 ReactDOM.render(
-    app,
+    <App title="React 16.8"/>,
     document.querySelector('#root')
 )
