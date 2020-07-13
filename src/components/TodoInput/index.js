73438b113a436c941c9115f36b82cbced957bb53
panda8z <panda8@126.com>
2020-07-13 15:55:22 +0800

    更改代码顺序。养成整洁代码习惯

--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -4,13 +4,17 @@ import PropTypes from 'prop-types';
 
 export default class TodoInput extends Component {
 
+    //静态类型检查
+    static propsTypes = {
+        btnText: PropTypes.string
+    }
+
+    // 可以给属性设置默认值
     static defaultProps = {
         btnText: '添加Todo'
     }
 
-    static propsTypes = {
-        btnText: PropTypes.string
-    }
+
     render() {
         return (
             <div>
