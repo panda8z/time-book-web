cc9bbe21adbf45df52bf55c46404849a2a7f0f75
panda8z <panda8@126.com>
2020-07-13 09:58:44 +0800

    使用 render 方法 使用类定义组件。

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
 
-// 这是一个箭头函数定义的方法。
-const App = (props) => { 
-    return (<div>
-    <h1>Welcom!</h1>
-    {/* jsx里面的js代码用 {} 包裹 */}
-    <p>{props.title}</p>
-</div>)
+
+class App extends React.Component {
+
+    render() {
+        console.log(this.props)
+        return (<div>
+            <h1>Welcom!</h1>
+            {/* jsx里面的js代码用 {} 包裹 */}
+            <p>{this.props.title}</p>
+        </div>)
+    }
 }
 
-ReactDOM.render(
-    <App title="React 16.8"/>,
+render(
+    <App title='React 16.8'/>,
     document.querySelector('#root')
 )
