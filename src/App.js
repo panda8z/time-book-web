283ba8d6ff51c172af665d4a537895246b9bccb3
panda8z <panda8@126.com>
2020-07-13 16:46:47 +0800

    总结组件类型，探索类中的constructor的使用

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { Component} from 'react';
-import {TodoHeader, TodoInput,TodoList }from './components';
+import React, { Component } from 'react';
+import { TodoHeader, TodoInput, TodoList } from './components';
+
+// 函数式组件 ，是无状态组件，因为他们有state 没有办法在组件内部自己修改自己。所以也叫它受控组件。
+// 类组件，是有状态组件，因为他有 props 和state 来接管外部通信和内部状态。
 
 export default class App extends Component {
-  render() {
-    console.log(this.props)
-    return (<>
-       <TodoHeader desc="今日事今日必" x={1} y={2}>
-       Todo事项
-       </TodoHeader>
-       <TodoInput />
-       <TodoList/>
-    </>);
-  }
+
+    // state 是组件本身的内部状态
+    // props 是组件间通信用的
+
+    constructor(props) {
+        super(props) // 使用 constructor 一定要调用super(props)
+        this.state = {
+            title: '待办事项',
+            desc: '今日事今日必'
+        }
+    }
+    render() {
+        console.log(this.props)
+        return (<>
+            <TodoHeader desc={this.state.desc} x={1} y={2}>
+                {this.state.title}
+            </TodoHeader>
+            <TodoInput />
+            <TodoList />
+        </>);
+    }
 }
 
 
