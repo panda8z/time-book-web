62eab3251a06ac21cb150f570bacebd5c21547bc
panda8z <panda8@126.com>
2020-07-15 08:46:22 +0800

    给 List加上参数检查

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 
 import TodoItem from './TodoItem';
 
+import PropTypes from 'prop-types'
+
 export default class TodoList extends Component {
+
+    static propTypes = {
+        todos: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            isCompleted: PropTypes.bool.isRequired
+        }))
+    }
 
     constructor(props) {
         super(props)
+        this.state = {
 
+        }
     }
 
     render() {
