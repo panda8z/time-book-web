d28eced7dd6dd22a18733839e2f1e8e23867e68d
panda8z <panda8@126.com>
2020-07-16 17:24:42 +0800

    解释总结 redux 手动使用

--- src/store.js
+++ src/store.js
@@ -3,3 +3,27 @@ import { createStore } from 'redux'
 import rootReducer from './reducers'
 
 export default createStore(rootReducer)
+
+/*
+
+Redux 的 store包含了如下成员
+
+dispatch
+subscribe
+getState
+replaceReducer
+Symble（observable）
+
+使用 redux的步骤
+1. yarn add redux 
+2. 创建 actionType 和 Action
+3. 创建 Reducer
+4. 创建 Store
+
+5. 组件中导入 action
+6. 通过props一步步往下传，在组件中拿到store
+7. 使用store拿出state渲染初始页面。
+8. 使用store的subscribe 方法订阅 store改变state。
+9. 在组件中调用dispatch方法发送相应的action
+10. 在reducer中监听相应的dispacth-action，作出state的更改。
+ */
